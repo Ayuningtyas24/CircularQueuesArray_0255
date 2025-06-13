d411b32bf848b5500be4d58f6d7da85caed9571e
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-06-13 08:26:57 +0700

    menambahkan fungsi remove

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -22,25 +22,51 @@ public:
 
         // Cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
-            {
-                cout << "\nQueue overflow\n";
-                return;
-            }
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
 
         // Cek apakah antrian kosong
-        if (FRONT == -1) 
+        if (FRONT == -1)
         {
             FRONT = 0;
             REAR = 0;
         }
-        else 
+        else
         {
-            //Jika REAR berada di posisi terakhir array, kembali ke awal array
+            // Jika REAR berada di posisi terakhir array, kembali ke awal array
             if (REAR == max - 1)
                 REAR = 0;
             else
                 REAR = REAR + 1;
         }
         queue_array[REAR] = num;
+    }
+
+    void remove()
+    {
+        // Cek apakah antrian kosong
+        if (FRONT == 1)
+        {
+            cout << "Queuenunderflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT];
+
+            // Cek jika antrian hanya memiliki satu elemen
+            if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 }
