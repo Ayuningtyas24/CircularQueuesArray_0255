91f0f014b1f70dd0863b9b271ce77e16232d72d3
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-06-13 09:46:24 +0700

    memanggil pilihan 4 dan default

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -152,8 +152,17 @@ int main()
                 q.display();
                 break;
             }
-    }
-}   
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
+            }
+        }
 }
 }
 
